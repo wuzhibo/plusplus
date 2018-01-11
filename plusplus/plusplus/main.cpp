3af2605e0668e6a429d4927cfe34d35628d7d453
wuzhibo <wuzhibo@bestitu.com>
2018-01-11 15:29:45 +0800

    添加基本的main文件

---
+++ plusplus/plusplus/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+
+
+
+
+
+inline double squrare(double x) { return x * x; }
+int main()
+{
+	using namespace std;
+	double a, b;
+	double c = 3.0;
+	a = squrare(5.0);
+	b = squrare(4.5 + 7.5);
+}
